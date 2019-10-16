a46e9afb5a331cc9262a29c93fafd74df387e5fb
Jean Pretorius <jean.pretorius@patternmatched.com>
2019-10-16 16:58:08 +0200

    This is my Third Commit

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create a React component
+const App = function() {
+    return <div> Hi There</div>;
+};
 
 // Take the React component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
